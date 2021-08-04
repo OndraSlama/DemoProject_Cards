40dc1a2153389d4a69f189aec1e6021fb9a915e1
Ondřej Sláma <ondrej.slama@rieter.com>
2021-08-04 10:47:12 +0200

    Basic deck imlemented

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+import dataclasses
 
 SUPPORTED_RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 SUPPORTED_SUITS = {"Spades", "Hearts", "Diamonds", "Clubs"}
 
 @dataclass
 class Card:
+    """ Single card implementation """
     rank: str
     suit: str
 
@@ -30,11 +32,26 @@ class Card:
         return f"{self.rank} of {self.suit}"
 
 
+@dataclass(init=False)
+class Deck():
+    """ Deck implementation"""
+    cards: list   
+
+    def __init__(self):
+        self.cards = [Card(rank, suit) for rank in SUPPORTED_RANKS for suit in SUPPORTED_SUITS]
+
+    def __str__(self) -> str:
+        return f"Deck of {len(self.cards)} cards :{[str(c) for c in self.cards]}" 
+        
+        
+
+
 
 def main():
-    card = Card("Ace", "Spades")
 
-    print(card)
+    deck = Deck()
+
+    print(deck)
 
 if __name__ == "__main__":
     main()
